3a38cf3c60059e946a095ab5be3f23e363c78ad5
devantler <nikolaiemildamm@icloud.com>
2022-07-24 22:21:21 +0200

    Removed unnecessary scripts and did some more work on ansible playbook to bootstrap nodes

--- scripts/linux-kernel/build-raspbian-aarch64.sh
+++
@@ -1,6 +0,0 @@
-make -j4 Image.gz modules dtbs
-sudo make modules_install
-sudo cp arch/arm64/boot/dts/broadcom/*.dtb /boot/
-sudo cp arch/arm64/boot/dts/overlays/*.dtb* /boot/overlays/
-sudo cp arch/arm64/boot/dts/overlays/README /boot/overlays/
-sudo cp arch/arm64/boot/Image.gz /boot/$KERNEL.img
--- scripts/linux-kernel/download-raspbian-aarch64.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install git bc bison flex libssl-dev make -y
-git clone --depth=1 https://github.com/raspberrypi/linux
-cd linux
-KERNEL=kernel8
-make bcm2711_defconfig
