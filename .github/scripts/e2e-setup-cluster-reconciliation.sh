094e176469d424d73260e150d51d898c0edf3c6e
devantler <nikolaiemildamm@icloud.com>
2023-07-25 12:04:36 +0200

    Fix git-ignore paths

--- .github/scripts/e2e-setup-cluster-reconciliation.sh
+++ .github/scripts/e2e-setup-cluster-reconciliation.sh
@@ -2,10 +2,9 @@ repository_url=$1
 branch_name=$2
 
 flux create source git flux-system \
---url=$repository_url \
---branch=$branch_name \
---ignore-paths="./k8s/clusters/development/flux-system/" \
---ignore-paths="./k8s/clusters/production/flux-system/"
-flux create kustomization flux-system \
---source=flux-system \
---path=./k8s/clusters/development
+    --url=$repository_url \
+    --branch=$branch_name \
+    --ignore-paths="k8s/clusters/**/flux-system/" \
+    flux create kustomization flux-system \
+    --source=flux-system \
+    --path=./k8s/clusters/development
