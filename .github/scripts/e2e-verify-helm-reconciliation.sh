64f1d22490ef8d821beb66a87380f1e01e228bd1
devantler <nikolaiemildamm@icloud.com>
2023-07-25 16:45:20 +0200

    Fixed the helm reconciliation script to auto find the helmreleases in the repo

--- .github/scripts/e2e-verify-helm-reconciliation.sh
+++ .github/scripts/e2e-verify-helm-reconciliation.sh
@@ -1,1 +1,11 @@
-# kubectl -n ingress-traefik wait helmrelease/ingress-traefik --for=condition=ready --timeout=5m
+helmreleases=$(find k8s -type f -name "release.yaml" | grep -E "k8s\/(.+\/)?(development|base)\/" | sort -u)
+if [[ -z "$helmreleases" ]]; then
+    echo "No helmreleases found in k8s/**/development or k8s/**/base"
+    exit 1
+fi
+
+for helmrelease in $helmreleases; do
+    namespace=$(yq eval '.metadata.namespace' "$helmrelease")
+    release_name=$(yq eval '.metadata.name' "$helmrelease")
+    kubectl -n "$namespace" wait "$release_name" --for=condition=ready --timeout=5m
+done
