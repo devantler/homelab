24878d7725f28808ac5a86eda099ce1817475d5f
devantler <nikolaiemildamm@icloud.com>
2023-07-26 03:33:27 +0200

    fixes

--- .github/scripts/e2e-verify-helm-reconciliation.sh
+++ .github/scripts/e2e-verify-helm-reconciliation.sh
@@ -1,10 +1,10 @@
-helmreleases=$(find k8s -type f -name "release.yaml" | grep -E "k8s\/(.+\/)?(development|base)\/" | sort -u)
-if [[ -z "$helmreleases" ]]; then
-    echo "No helmreleases found in k8s/**/development or k8s/**/base"
+helm_releases=$(find k8s -type f -name "release.yaml" | grep -E "k8s\/(.+\/)?(base)\/" | sort -u)
+if [[ -z "$helm_releases" ]]; then
+    echo "No Helm releases found in k8s/**/base"
 else
-    for helmrelease in $helmreleases; do
-        namespace=$(yq eval '.metadata.namespace' "$helmrelease")
-        release_name=$(yq eval '.metadata.name' "$helmrelease")
+    for helm_release in $helm_releases; do
+        namespace=$(yq eval '.metadata.namespace' "$helm_release")
+        release_name=$(yq eval '.metadata.name' "$helm_release")
         kubectl -n "$namespace" wait "$release_name" --for=condition=ready --timeout=5m
     done
 fi
