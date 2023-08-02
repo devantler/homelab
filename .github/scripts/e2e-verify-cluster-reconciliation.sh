103c1e1710c7ab50bf419640fb0c9de332260cb4
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2023-08-02 07:33:31 +0200

    Update e2e-verify-cluster-reconciliation.sh

--- .github/scripts/e2e-verify-cluster-reconciliation.sh
+++ .github/scripts/e2e-verify-cluster-reconciliation.sh
@@ -1,2 +1,2 @@
-kubectl -n flux-system wait kustomization/infrastructure --for=condition=ready --timeout=5m
-kubectl -n flux-system wait kustomization/apps --for=condition=ready --timeout=5m
+kubectl -n flux-system wait kustomization/infrastructure --for=condition=ready --timeout=2m
+kubectl -n flux-system wait kustomization/apps --for=condition=ready --timeout=2m
