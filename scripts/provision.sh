f52901721b75f6fa8e090c4c23e90a812e002c6e
devantler <nikolaiemildamm@icloud.com>
2023-08-15 21:10:25 +0200

    Fixes

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "🚀 Provision K3s development cluster"
-k3d cluster create -c cluster-configs/cluster-development.yml
+k3d cluster create -c cluster-configs/cluster-development.yaml
 
 echo "🚀 Provision K3s production cluster"
-k3d cluster create -c cluster-configs/cluster-production.yml
+k3d cluster create -c cluster-configs/cluster-production.yaml
