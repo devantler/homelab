bdbb6e3c6edfbf1f5c9392bab3036ecf4a37d406
devantler <nikolaiemildamm@icloud.com>
2023-08-16 23:39:23 +0200

    update

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,8 +2,10 @@
 
 echo "🚀 Provisioning K3s development cluster"
 k3d cluster create -c cluster-configs/cluster-development.yaml
+flux bootstrap github --owner=$GITHUB_USER --repository=homelab --branch=main --path=./k8s/clusters/development --personal
 
 echo -e "\n"
 
 echo "🚀 Provisioning K3s production cluster"
 k3d cluster create -c cluster-configs/cluster-production.yaml
+flux bootstrap github --owner=$GITHUB_USER --repository=homelab --branch=main --path=./k8s/clusters/production --personal
