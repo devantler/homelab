d79424580f2cc2b041d7aa95dc8798f5d600a9ec
devantler <nikolaiemildamm@icloud.com>
2023-08-22 00:03:55 +0200

    Added emojies

--- .github/scripts/validate-manifests.sh
+++ .github/scripts/validate-manifests.sh
@@ -11,16 +11,16 @@ kustomize_config="kustomization.yaml"
 kubeconform_flags=("-skip=Secret")
 kubeconform_config=("-strict" "-ignore-missing-schemas" "-schema-location" "default" "-schema-location" "/tmp/flux-crd-schemas" "-verbose")
 
-echo "INFO - Downloading Flux OpenAPI schemas"
+echo "🔍 INFO - Downloading Flux OpenAPI schemas"
 mkdir -p /tmp/flux-crd-schemas/master-standalone-strict
 curl -sL https://github.com/fluxcd/flux2/releases/latest/download/crd-schemas.tar.gz | tar zxf - -C /tmp/flux-crd-schemas/master-standalone-strict
 
 find . -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
-  echo "INFO - Validating $file"
+  echo "🔍 INFO - Validating $file"
   yq e 'true' "$file" >/dev/null
 done
 
-echo "INFO - Validating clusters"
+echo "🔍 INFO - Validating clusters"
 find ./k8s/clusters -maxdepth 2 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
   kubeconform "${kubeconform_flags[@]}" "${kubeconform_config[@]}" "${file}"
   if [[ ${PIPESTATUS[0]} != 0 ]]; then
@@ -28,9 +28,9 @@ find ./k8s/clusters -maxdepth 2 -type f -name '*.yaml' -print0 | while IFS= read
   fi
 done
 
-echo "INFO - Validating kustomize overlays"
+echo "🔍 INFO - Validating kustomize overlays"
 find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
-  echo "INFO - Validating kustomization ${file/%$kustomize_config/}"
+  echo "🔍 INFO - Validating kustomization ${file/%$kustomize_config/}"
   kustomize build "${file/%$kustomize_config/}" "${kustomize_flags[@]}" |
     kubeconform "${kubeconform_flags[@]}" "${kubeconform_config[@]}"
   if [[ ${PIPESTATUS[0]} != 0 ]]; then
