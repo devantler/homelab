ff48df1b9b66ff36ff9f8db0bf310ab57bbb36ed
devantler <nikolaiemildamm@icloud.com>
2023-08-24 18:52:59 +0200

    fix

--- scripts/bootstrap-local.sh
+++ scripts/bootstrap-local.sh
@@ -5,4 +5,4 @@ k3d cluster create cluster-local --config k3d-config.yaml
 
 echo "🚀 Installing Flux"
 flux check --pre
-flux bootstrap git --url=ssh://git@github.com/energinet-digitalisering/infrastructure.git --private-key-file=$HOME/.ssh/energinet_admin --password=$ENERGINET_DIGITAL_ADMIN_SSH_KEY_PASSWORD --path=k8s/clusters/local --branch=$branch
+flux bootstrap github --owner=$GITHUB_USER --repository=homelab --path=./k8s/clusters/local --personal --branch=$branch
