f9725332715e9603ef62b2dda1d880b5ce8641a6
devantler <nikolaiemildamm@icloud.com>
2023-08-27 20:52:32 +0200

    wip

--- .github/scripts/e2e-verify-helm-reconciliation.sh
+++ .github/scripts/e2e-verify-helm-reconciliation.sh
@@ -1,10 +1,32 @@
-helm_releases=$(find k8s -type f -name "release.yaml" | grep -E "k8s\/(.+\/)?(base)\/" | sort -u)
-if [[ -z "$helm_releases" ]]; then
-    echo "No Helm releases found in k8s/**/base"
-else
-    for helm_release in $helm_releases; do
-        namespace=$(yq eval '.metadata.namespace' "$helm_release")
-        name=helmrelease/$(yq eval '.metadata.name' "$helm_release")
+function test_helm_releases() {
+    local releases=()
+    local release_file=$(yq eval '.resources[] | select(. == "*release.yaml" )' "$1"/kustomization.yaml)
+    if [[ -n $release_file ]]; then
+        releases+=$(yq eval '.metadata.name' "$1"/$release_file)
+        namespace=$(yq eval '.metadata.namespace' "$1"/$release_file)
+        name=helmrelease/$(yq eval '.metadata.name' "$1"/$release_file)
         kubectl -n "$namespace" wait "$name" --for=condition=ready --timeout=5m
-    done
+    fi
+
+    local subdirectories=$(yq eval '.resources[] | select(. != "*.yaml" )' "$1"/kustomization.yaml)
+    if [[ -n $subdirectories ]]; then
+        for subdirectory in $subdirectories; do
+            test_helm_releases "$1/$subdirectory"
+        done
+    fi
+}
+
+if [ -z "$1" ]; then
+    echo "Error: 'environment' argument not set"
+    exit 1
 fi
+
+environment=$1
+
+apps_path=$(yq eval '.spec.path' k8s/clusters/$environment/apps.yaml)
+infrastructure_path=$(yq eval '.spec.path' k8s/clusters/$environment/infrastructure.yaml)
+configs_path=$(yq eval '.spec.path' k8s/clusters/$environment/configs.yaml)
+
+for path in $apps_path $infrastructure_path $configs_path; do
+    test_helm_releases $path
+done
