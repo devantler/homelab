717178423206dee2d637445730a544518ede841c
devantler <nikolaiemildamm@icloud.com>
2023-09-13 22:37:47 +0200

    Updated sops key

--- scripts/bootstrap-production.sh
+++ scripts/bootstrap-production.sh
@@ -3,6 +3,12 @@
 echo "☸️  Provisioning Kubernetes cluster"
 microk8s install -y
 
+echo "🔐 Adding SOPS GPG key"
+gpg --export-secret-keys --armor "1F1A648778E72857BD9CF481EE0834B3CEAC3061" |
+kubectl create secret generic sops-gpg \
+--namespace=flux-system \
+--from-file=sops.asc=/dev/stdin
+
 echo "🔼☸️  Upgrading Kubernetes cluster"
 kubectl drain microk8s-vm --ignore-daemonsets
 multipass exec microk8s-vm -- sudo snap refresh microk8s --channel latest/stable
