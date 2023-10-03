79e021365c1fcf7d49f46015e45b0e58d7570314
devantler <nikolaiemildamm@icloud.com>
2023-10-04 01:42:03 +0200

    update

--- .github/scripts/e2e-verify-cluster-reconciliation.sh
+++ .github/scripts/e2e-verify-cluster-reconciliation.sh
@@ -1,6 +1,14 @@
 start_time=$(date +%s)
+kubectl -n flux-system wait kustomization/post-build --for=condition=ready --timeout=5m
+echo "Time taken for post-build kustomization: $(($(date +%s) - $start_time)) seconds"
+
+start_time=$(date +%s)
 kubectl -n flux-system wait kustomization/infrastructure --for=condition=ready --timeout=5m
 echo "Time taken for infrastructure kustomization: $(($(date +%s) - $start_time)) seconds"
+
+start_time=$(date +%s)
+kubectl -n flux-system wait kustomization/infrastructure-configs --for=condition=ready --timeout=5m
+echo "Time taken for infrastructure-configs kustomization: $(($(date +%s) - $start_time)) seconds"
 
 start_time=$(date +%s)
 kubectl -n flux-system wait kustomization/apps --for=condition=ready --timeout=5m
