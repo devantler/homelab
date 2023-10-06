ca298e710eb3dcbfb5d30847a08ce1da838ff14c
devantler <nikolaiemildamm@icloud.com>
2023-10-06 22:43:31 +0200

    Update bootstrap script

--- scripts/bootstrap-production.sh
+++ scripts/bootstrap-production.sh
@@ -8,19 +8,24 @@ kubectl config use-context homelab
 echo "🔧 Patch machine-config on all control-plane nodes"
 talosctl patch mc \
     -n 10.0.0.201 \
+    --patch @machine-config.control-plane.yaml
+
+echo "🔧 Patch machine-config on all control-plane nodes with storage"
+talosctl patch mc \
     -n 10.0.0.202 \
     -n 10.0.0.203 \
-    --patch @machine-config.control-plane.yaml
+    --patch @machine-config.control-plane.storage.yaml
 
 echo "🔧 Patch machine-config on all worker nodes"
 echo "No worker nodes to patch yet"
 
-echo "🚀 Installing Flux"
-flux check --pre
-flux bootstrap github --owner=$GITHUB_USER --repository=homelab --path=./k8s/clusters/production --personal --branch=main
-
 echo "🔐 Adding SOPS GPG key"
+kubectl create namespace flux-system
 gpg --export-secret-keys --armor "1F1A648778E72857BD9CF481EE0834B3CEAC3061" |
     kubectl create secret generic sops-gpg \
         --namespace=flux-system \
         --from-file=sops.asc=/dev/stdin
+
+echo "🚀 Installing Flux"
+flux check --pre
+flux bootstrap github --owner=$GITHUB_USER --repository=homelab --path=./k8s/clusters/production --personal --branch=main
