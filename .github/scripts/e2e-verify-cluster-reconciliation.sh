ee3b8eef68174e578f9142211493d2a80d42ea41
devantler <nikolaiemildamm@icloud.com>
2023-10-09 00:02:30 +0200

    Make CI env like prod

--- .github/scripts/e2e-verify-cluster-reconciliation.sh
+++ .github/scripts/e2e-verify-cluster-reconciliation.sh
@@ -3,7 +3,7 @@ kubectl -n flux-system wait kustomization/post-build --for=condition=ready --tim
 echo "Time taken for post-build kustomization: $(($(date +%s) - $start_time)) seconds"
 
 start_time=$(date +%s)
-kubectl -n flux-system wait kustomization/infrastructure --for=condition=ready --timeout=10m || exit 1
+kubectl -n flux-system wait kustomization/infrastructure --for=condition=ready --timeout=5m || exit 1
 echo "Time taken for infrastructure kustomization: $(($(date +%s) - $start_time)) seconds"
 
 start_time=$(date +%s)
