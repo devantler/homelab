c5ece8485dd097b10c79c3516859091e12e5956b
devantler <nikolaiemildamm@icloud.com>
2023-10-13 22:48:44 +0200

    update

--- scripts/destroy-sandbox.sh
+++ scripts/destroy-sandbox.sh
@@ -3,4 +3,5 @@
 pushd $(dirname "$0") >/dev/null
 
 echo "🔥 Destroy Sandbox"
-talosctl cluster destroy --name homelab-sandbox
+talosctl cluster destroy --name homelab-sandbox --force
+kubectl config delete-context admin@homelab-sandbox
