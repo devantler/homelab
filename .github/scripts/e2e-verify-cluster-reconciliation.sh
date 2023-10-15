2af1dda02df31242ee0d5887451c158df9f5927d
devantler <nikolaiemildamm@icloud.com>
2023-10-15 22:00:15 +0200

    fixes

--- .github/scripts/e2e-verify-cluster-reconciliation.sh
+++ .github/scripts/e2e-verify-cluster-reconciliation.sh
@@ -16,9 +16,9 @@ kubectl -n flux-system wait kustomization/infrastructure-configs --for=condition
 echo "Time taken for kustomization/infrastructure-configs: $(($(date +%s) - $start_time)) seconds"
 
 start_time=$(date +%s)
-echo "Starting reconciliation of kustomization/infrastructure-secrets..."
+echo "Starting reconciliation of kustomization/apps..."
 kubectl -n flux-system wait kustomization/apps --for=condition=ready --timeout=5m || exit 1
 echo "Time taken for kustomization/apps: $(($(date +%s) - $start_time)) seconds"
 
-kubectl -n flux-system wait kustomization/flux-system --for=condition=ready --timeout=5m || exit 1
+kubectl -n flux-system wait kustomization/flux-system --for=condition=ready --timeout=1m || exit 1
 echo "Time taken for kustomization/flux-system: $(($(date +%s) - $global_start_time)) seconds"
