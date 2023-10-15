137499c854f2fddb2b442ad2acd5eb7a20cb0d52
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2023-10-15 23:29:02 +0200

    Update documentation (#405)

--- .github/scripts/e2e-debug-failure.sh
+++ .github/scripts/e2e-debug-failure.sh
@@ -23,3 +23,5 @@ if kubectl -n flux-system logs deploy/helm-controller | grep -i 'error'; then
     echo "📝 Flux logs - helm-controller errors"
     kubectl -n flux-system logs deploy/helm-controller | grep -i 'error'
 fi
+
+kubectl describe helmreleases -A
