e0df162056eae114571d54fef8496259b8ee05a1
devantler <nikolaiemildamm@icloud.com>
2023-10-19 20:51:51 +0200

    fix

--- .github/scripts/e2e-check-deprecated-apis.sh
+++ .github/scripts/e2e-check-deprecated-apis.sh
@@ -4,4 +4,4 @@ release=$(curl -sL "https://api.github.com/repos/doitintl/kube-no-trouble/releas
 os=$(uname -s | tr '[:upper:]' '[:lower:]')
 arch=$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/')
 curl -L -o- "https://github.com/doitintl/kube-no-trouble/releases/download/${release}/kubent-${release}-${os}-${arch}.tar.gz" | tar xvzf -
-./kubent
+./kubent -e
