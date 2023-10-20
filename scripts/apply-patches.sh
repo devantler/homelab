7409a138d45e097924a1a26e8f5b8e61099b3fe9
devantler <nikolaiemildamm@icloud.com>
2023-10-20 22:40:02 +0200

    Update script

--- scripts/apply-patches.sh
+++ scripts/apply-patches.sh
@@ -1,11 +1,11 @@
-echo "Apply cluster wide patches"
+echo "🩹 Apply cluster wide patches"
 
-echo "Apply controlplane patches"
+echo "🩹 Apply controlplane patches"
 
-echo "Apply worker patches"
+echo "🩹 Apply worker patches"
 
-echo "Apply talos-rpi-1 patches"
+echo "🩹 Apply talos-rpi-1 patches"
 
-echo "Apply talos-rpi-2 patches"
+echo "🩹 Apply talos-rpi-2 patches"
 
-echo "Apply talos-rpi-3 patches"
+echo "🩹 Apply talos-rpi-3 patches"
