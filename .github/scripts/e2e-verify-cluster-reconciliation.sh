c5b5477b9d42a2b7543ffa0550f2ec9d7a9832b9
devantler <nikolaiemildamm@icloud.com>
2023-10-30 03:35:13 +0100

    set timeout back to 5m

--- .github/scripts/e2e-verify-cluster-reconciliation.sh
+++ .github/scripts/e2e-verify-cluster-reconciliation.sh
@@ -7,7 +7,7 @@ echo "Time taken for kustomization/variables: $(($(date +%s) - $start_time)) sec
 
 start_time=$(date +%s)
 echo "Starting reconciliation of kustomization/infrastructure..."
-kubectl -n flux-system wait kustomization/infrastructure --for=condition=ready --timeout=7m || exit 1
+kubectl -n flux-system wait kustomization/infrastructure --for=condition=ready --timeout=5m || exit 1
 echo "Time taken for kustomization/infrastructure: $(($(date +%s) - $start_time)) seconds"
 
 start_time=$(date +%s)
