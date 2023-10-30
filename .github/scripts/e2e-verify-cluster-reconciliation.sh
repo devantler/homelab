2f8721c1d22fe762db28b8ef6ff286220aaa2fde
devantler <nikolaiemildamm@icloud.com>
2023-10-30 03:20:05 +0100

    Change timeouts

--- .github/scripts/e2e-verify-cluster-reconciliation.sh
+++ .github/scripts/e2e-verify-cluster-reconciliation.sh
@@ -7,7 +7,7 @@ echo "Time taken for kustomization/variables: $(($(date +%s) - $start_time)) sec
 
 start_time=$(date +%s)
 echo "Starting reconciliation of kustomization/infrastructure..."
-kubectl -n flux-system wait kustomization/infrastructure --for=condition=ready --timeout=5m || exit 1
+kubectl -n flux-system wait kustomization/infrastructure --for=condition=ready --timeout=7m || exit 1
 echo "Time taken for kustomization/infrastructure: $(($(date +%s) - $start_time)) seconds"
 
 start_time=$(date +%s)
@@ -17,7 +17,7 @@ echo "Time taken for kustomization/infrastructure-configs: $(($(date +%s) - $sta
 
 start_time=$(date +%s)
 echo "Starting reconciliation of kustomization/apps..."
-kubectl -n flux-system wait kustomization/apps --for=condition=ready --timeout=5m || exit 1
+kubectl -n flux-system wait kustomization/apps --for=condition=ready --timeout=1m || exit 1
 echo "Time taken for kustomization/apps: $(($(date +%s) - $start_time)) seconds"
 
 kubectl -n flux-system wait kustomization/flux-system --for=condition=ready --timeout=1m || exit 1
