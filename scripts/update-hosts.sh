a0cff80c6b4ece7ceb1598d8a9607d6da3af89b5
devantler <nikolaiemildamm@icloud.com>
2024-03-05 22:07:35 +0100

    Init

--- scripts/update-hosts.sh
+++
@@ -1,7 +0,0 @@
-echo "🌐 Add host names to /etc/hosts"
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  sudo sed -i '' '/### --- Homelab START --- ###/,/### --- Homelab END --- ###/d' /etc/hosts
-  cat ./hosts >>/etc/hosts
-else
-  echo "This script is only supported on macOS at the moment... Feel free to contribute support for your OS!"
-fi
